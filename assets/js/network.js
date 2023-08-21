1a9d166e6d621cf2547157df12bdf8faf6b1c39e
luupham1307 <hoangluupham1307@gmail.com>
2023-08-21 11:52:22 +0700

    add collection

---
+++ assets/js/network.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".network-img", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    // centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    // loop: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
+    },
+
+});
